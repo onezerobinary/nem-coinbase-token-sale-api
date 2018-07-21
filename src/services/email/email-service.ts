50c5995e5c293addcfc5193d4cd4dcde1a9f0904
Enrico Zanardo <ezanardo@onezerobinary.com>
2018-07-21 17:13:51 +0200

    Update data for email service

--- src/services/email/email-service.ts
+++ src/services/email/email-service.ts
@@ -3,33 +3,35 @@ import { createTransport, SentMessageInfo } from 'nodemailer';
 const emailCreds = require('./emailCreds.json');
 
 export interface Email {
-	to: string;
-	subject: string;
-	html: string;
+    from: string;
+    to: string;
+    subject: string;
+    html: string;
 }
 
 export interface EmailAttachment {
-	to: string;
-	bcc?: string;
-	subject: string;
-	html: string;
-	attachments: Array<Attachment>;
+    from: string;
+    to: string;
+    bcc?: string;
+    subject: string;
+    html: string;
+    attachments: Array<Attachment>;
 }
 
 export interface Attachment {filename: string; path: string; cid: string; }
 
 // export const mailTransport: any =  {
-// 	service: 'Gmail',
-// 	auth: {
-// 		type: 'OAuth2',
-// 		user: emailCreds.kycEmail.user,
-// 		clientId: emailCreds.kycEmail.clientId,
-// 		clientSecret: emailCreds.kycEmail.clientSecret,
-// 		refreshToken: emailCreds.kycEmail.refreshToken
-// 	}
+// service: 'Gmail',
+// auth: {
+// 	type: 'OAuth2',
+// 	user: emailCreds.kycEmail.user,
+// 	clientId: emailCreds.kycEmail.clientId,
+// 	clientSecret: emailCreds.kycEmail.clientSecret,
+// 	refreshToken: emailCreds.kycEmail.refreshToken
+// }
 // };
 
-export const onezerobinary_mailTransport: any =  {
+export const transporter: any =  {
     host:  emailCreds.onezerobinary.host,
     port: emailCreds.onezerobinary.port,
     auth: {
@@ -41,9 +43,10 @@ export const onezerobinary_mailTransport: any =  {
 };
 
 export const sendEmail = async (mailOptions: Email): Promise<SentMessageInfo> => {
-	return createTransport(onezerobinary_mailTransport).sendMail(mailOptions);
+    // return createTransport(mailTransport).sendMail(mailOptions);
+    return createTransport(transporter).sendMail(mailOptions);
 };
 
 export const sendKYCEmail = async (mailOptions: EmailAttachment): Promise<SentMessageInfo> => {
-	return createTransport(onezerobinary_mailTransport).sendMail(mailOptions);
+    return createTransport(transporter).sendMail(mailOptions);
 };
--- src/services/email/templates/kyc-template.ts
+++ src/services/email/templates/kyc-template.ts
@@ -1,23 +1,27 @@
 import { EmailAttachment } from 'services/email/email-service';
 import { Request } from 'express';
 import {
-	KEY_ADDRESS_1, KEY_ADDRESS_2,
-	KEY_CITY, KEY_COUNTRY,
-	KEY_FULL_NAME,
-	KEY_LEGAL_PHOTO_ID_PATH,
-	KEY_PERSONAL_PICTURE_HOLDING_ID_PATH,
-	KEY_PROOF_OF_RESIDENCE_PATH,
-	KEY_TOKEN_RECIPIENT_ADDRESS,
-	KEY_STATE,
-	KEY_ZIP
+    KEY_ADDRESS_1, KEY_ADDRESS_2,
+    KEY_CITY, KEY_COUNTRY,
+    KEY_FULL_NAME,
+    KEY_LEGAL_PHOTO_ID_PATH,
+    KEY_PERSONAL_PICTURE_HOLDING_ID_PATH,
+    KEY_PROOF_OF_RESIDENCE_PATH,
+    KEY_TOKEN_RECIPIENT_ADDRESS,
+    KEY_STATE,
+    KEY_ZIP,
+    KEY_PRODUCT_ID
 } from 'utilities/validators';
+
+const emailCreds = require('../emailCreds.json');
 
 export const kycTemplate = (req: Request): EmailAttachment => {
 
-	const email: EmailAttachment = {
-		to: process.env.KYC_EMAILS,
-		subject: `KYC Form for ${req.body[KEY_FULL_NAME]}`,
-		html: `<p>Name: ${req.body[KEY_FULL_NAME]}</p>
+    const email: EmailAttachment = {
+        from: emailCreds.kycEmail.user,
+        to: process.env.KYC_EMAILS,
+        subject: `KYC Form for ${req.body[KEY_FULL_NAME]}`,
+        html: `<p>Name: ${req.body[KEY_FULL_NAME]}</p>
 						<p>Address 1: ${req.body[KEY_ADDRESS_1]}</p>
 						<p>Address 2: ${req.body[KEY_ADDRESS_2]}</p>
 						<p>City: ${req.body[KEY_CITY]}</p>
@@ -25,27 +29,26 @@ export const kycTemplate = (req: Request): EmailAttachment => {
 						<p>ZIP/Postal Code: ${req.body[KEY_ZIP]}</p>
 						<p>Country: ${req.body[KEY_COUNTRY]}</p>
 						<p>Token Recipient Address: ${req.body[KEY_TOKEN_RECIPIENT_ADDRESS]}</p>
-						<p><img src="cid:legalPhotoId@blockstart.io"/></p>
-						<p><img src="cid:personalPicture@blockstart.io"/></p>
-						<p><img src="cid:proofResidence@blockstart.io"/></p>`,
-		attachments: [
-			{
-				filename: 'legalPhotoId.png',
-				path: req.body[KEY_LEGAL_PHOTO_ID_PATH].path,
-				cid: 'legalPhotoId@blockstart.io'
-			},
-			{
-				filename: 'personalPicture.png',
-				path: req.body[KEY_PERSONAL_PICTURE_HOLDING_ID_PATH].path,
-				cid: 'personalPicture@blockstart.io'
-			},
-			{
-				filename: 'proofResidence.png',
-				path: req.body[KEY_PROOF_OF_RESIDENCE_PATH].path,
-				cid: 'proofResidence@blockstart.io'
-			}
-		]
-	};
+						<p>Product ID: ${req.body[KEY_PRODUCT_ID]}</p>
+				`,
+        attachments: [
+            {
+                filename: 'legalPhotoId.png',
+                path: req.body[KEY_LEGAL_PHOTO_ID_PATH].path,
+                cid: 'legalPhotoId@onezerobinary.com'
+            },
+            {
+                filename: 'personalPicture.png',
+                path: req.body[KEY_PERSONAL_PICTURE_HOLDING_ID_PATH].path,
+                cid: 'personalPicture@onezerobinary.com'
+            },
+            {
+                filename: 'proofResidence.png',
+                path: req.body[KEY_PROOF_OF_RESIDENCE_PATH].path,
+                cid: 'proofResidence@onezerobinary.com'
+            }
+        ]
+    };
 
-	return email;
+    return email;
 };
