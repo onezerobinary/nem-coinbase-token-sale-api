7226e832ae0f6e4ec9d910e2bf3c6054db4f3d21
Enrico Zanardo <ezanardo@onezerobinary.com>
2018-07-21 17:01:25 +0200

    Use a private email server.

--- src/services/email/email-service.ts
+++ src/services/email/email-service.ts
@@ -18,21 +18,32 @@ export interface EmailAttachment {
 
 export interface Attachment {filename: string; path: string; cid: string; }
 
-export const mailTransport: any =  {
-	service: 'Gmail',
-	auth: {
-		type: 'OAuth2',
-		user: emailCreds.kycEmail.user,
-		clientId: emailCreds.kycEmail.clientId,
-		clientSecret: emailCreds.kycEmail.clientSecret,
-		refreshToken: emailCreds.kycEmail.refreshToken
-	}
+// export const mailTransport: any =  {
+// 	service: 'Gmail',
+// 	auth: {
+// 		type: 'OAuth2',
+// 		user: emailCreds.kycEmail.user,
+// 		clientId: emailCreds.kycEmail.clientId,
+// 		clientSecret: emailCreds.kycEmail.clientSecret,
+// 		refreshToken: emailCreds.kycEmail.refreshToken
+// 	}
+// };
+
+export const onezerobinary_mailTransport: any =  {
+    host:  emailCreds.onezerobinary.host,
+    port: emailCreds.onezerobinary.port,
+    auth: {
+        user: emailCreds.onezerobinary.user,
+        pass: emailCreds.onezerobinary.pass
+    },
+    tls: {rejectUnauthorized: false},
+    debug: true
 };
 
 export const sendEmail = async (mailOptions: Email): Promise<SentMessageInfo> => {
-	return createTransport(mailTransport).sendMail(mailOptions);
+	return createTransport(onezerobinary_mailTransport).sendMail(mailOptions);
 };
 
 export const sendKYCEmail = async (mailOptions: EmailAttachment): Promise<SentMessageInfo> => {
-	return createTransport(mailTransport).sendMail(mailOptions);
+	return createTransport(onezerobinary_mailTransport).sendMail(mailOptions);
 };
